1dff5558a029d4b83e9a8bb177e20565d2a6db61
Matt Hamilton <matt.hamilton@berkeley.edu>
2024-06-04 14:56:53 -0700

    game refactored - finished

--- day11/blackjack.py
+++ day11/blackjack.py
@@ -1,6 +1,11 @@
 import random as r
 import os
+import emoji
+import time
+
 def blackjack():
+
+    
 
     def drawCard():
         return r.randint(2,10)
@@ -11,72 +16,111 @@ def blackjack():
     def getScore(arr):
         total = 0
         for i in arr:
-            total+=i
+            total+=int(i)
         return total
- 
-    gameOver = 'Y'
+    def getComputerCardsHidden(cards):
+        hiddenCards = [str(cards[0])]
+        [hiddenCards.append("?") for _ in range(len(cards) - 1)]
+        return hiddenCards
+
+    
+
+    
+    def displayMidResults():
+        print(f'Your cards: {userCards}, Current Score: {getScore(userCards)}')
+        print(f'Computer First Card: {getComputerCardsHidden(computerCards)}')
+        print('-------------------------------------------')
+        
+    def displayFinalResults():
+        os.system('cls')
+        time.sleep(1)
+        print(f'Your final hand: {userCards}, Current Score: {getScore(userCards)}')
+        time.sleep(1)
+        print(f"Computer's final hand : {computerCards}, Current Score: {getScore(computerCards)}")
+
+        if winner == 'user':
+            print(f"Congrats. You won! {emoji.emojize(':beaming_face_with_smiling_eyes:')}")
+        elif winner == 'draw':
+            print(f"Draw! No winners this time {emoji.emojize(':winking_face:')}")
+        else:
+            print(f"You Lost {emoji.emojize(':smiling_face_with_tear:')}")
+
+    
     userCards = beginGame()
     computerCards = beginGame()
+    winner = ""
+        
 
-    gameOver = input("Do you want to play Blackjack? (Y/N): ").upper()
-  
-    while gameOver == 'Y':
+    play = input(f"Do you want to play Blackjack? {emoji.emojize(':club_suit:')} (Y/N): ").upper()
+
+    os.system('cls')
+    print('shuffling cards .')
+    time.sleep(1)
+    os.system('cls')
+    print('shuffling cards ..')
+    time.sleep(1)
+    os.system('cls')
+    print('shuffling cards ...')
+    time.sleep(1)
+    os.system('cls')
+
+    while play == 'Y':
         os.system('cls')
 
-        print(f'Your cards: {userCards}, Current Score: {getScore(userCards)}')
-        print(f'Computer First Card: {computerCards[0]}')
-        print('-------------------------------------------')
-        draw = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+        displayMidResults()
+        getNewCard = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+        os.system('cls')
 
-        if draw == 'y':
-            userCards.append(drawCard())
-            if getScore(computerCards) + 4 < 21:
-                computerCards.append(drawCard())
-        else:
-            if getScore(computerCards) + 4 < 21:
-                computerCards.append(drawCard())
+        #user draws cards as long as needs
+        while getNewCard == 'y':
+            if getNewCard == 'y':
+                userCards.append(drawCard())
+                #computer player also takes cards if needed
+                if getScore(computerCards) + 4 <= 21:
+                    computerCards.append(drawCard())
+                    print(f"Computer player drew one more card {emoji.emojize(':thinking_face:')}")
+                    time.sleep(1)
+
+            displayMidResults()
+            getNewCard = input("Type 'y' to get another card, type 'n' to pass: ").lower()
             os.system('cls')
-            print(f'Your final hand: {userCards}, Current Score: {getScore(userCards)}')
-            print(f"Computer's final hand : {computerCards}, Current Score: {getScore(computerCards)}")
+            
+        #check if the computer player needs more cards - to make the game more fair
+        while getScore(computerCards) + 4 <= 21:
+            computerCards.append(drawCard())
+            print(f"Computer player drew one more card {emoji.emojize(':thinking_face:')}")
+            time.sleep(1)
 
-            if getScore(userCards) > 21 and getScore(computerCards) <= 21:
-                print('You lost')
-                gameOver = input("Do you want to play Blackjack again? (Y/N): ").upper()
-                if gameOver == 'Y':
-                    userCards = beginGame()
-                    computerCards = beginGame()
-                    
+        #COMPUTE RESULTS
+        if getScore(userCards) <= 21 and getScore(computerCards) <= 21:
 
-            if getScore(computerCards) > 21 and getScore(userCards) <= 21:
-                print('Congrats! You WON')
-                gameOver = input("Do you want to play Blackjack again? (Y/N): ").upper()
-                if gameOver == 'Y':
-                    userCards = beginGame()
-                    computerCards = beginGame()
-            elif getScore(userCards) <= 21 and getScore(userCards) > getScore(computerCards):
-                print('Congrats! You WON')
-                gameOver = input("Do you want to play Blackjack again? (Y/N): ").upper()
-                if gameOver == 'Y':
-                    userCards = beginGame()
-                    computerCards = beginGame()
-            elif getScore(userCards) <= 21 and getScore(userCards) == getScore(computerCards):
-                print('Draw')
-                gameOver = input("Do you want to play Blackjack again? (Y/N): ").upper()
-                if gameOver == 'Y':
-                    userCards = beginGame()
-                    computerCards = beginGame()
-            elif getScore(computerCards) > 21 and getScore(userCards) > 21:
-                print("No winners this time")
-                gameOver = input("Do you want to play Blackjack again? (Y/N): ").upper()
-                if gameOver == 'Y':
-                    userCards = beginGame()
-                    computerCards = beginGame()
+            if getScore(userCards) > getScore(computerCards):
+                winner = 'user'
+            elif getScore(userCards) == getScore(computerCards):
+                winner = 'draw'
             else:
-                print('Sorry buddy, You Lost!')
-                gameOver = input("Do you want to play Blackjack again? (Y/N): ").upper()
-                if gameOver == 'Y':
-                    userCards = beginGame()
-                    computerCards = beginGame()
+                winner = 'computer'
+
+        elif getScore(userCards) <= 21 and getScore(computerCards) > 21:
+            winner = 'user'
+        elif  getScore(userCards) > 21 and getScore(computerCards) > 21:
+            winner = 'draw'
+        else:
+            winner = 'computer'
+
+
+        #DISAPLAY THE RESULTS 
+        displayFinalResults()
+
+        play = input('Would you like to play again? (y/n) :').upper()
+
+        #RESET
+        userCards = beginGame()
+        computerCards = beginGame()
+        winner = ""
+
+
+
                 
         
          
