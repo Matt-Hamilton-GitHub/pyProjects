a59d54786ab68339df021f95d981c5a3fed38080
Matt <matt.hamilton@berkeley.edu>
2024-06-13 21:00:15 -0700

    day45 complete

--- day45/main.py
+++ day45/main.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 
-with open("website.html", encoding='UTF8') as file:
-    contents = file.read()
+# with open("website.html", encoding='UTF8') as file:
+#     contents = file.read()
 
 #BeatifulSoup(content, parser), can be lxml -> 'lxml' 
 # requres installing lxml library
@@ -22,21 +22,42 @@ with open("website.html", encoding='UTF8') as file:
 
 
 #Live Website
-res = requests.get('https://news.ycombinator.com/news')
-yc_web_page = res.text
-soup = BeautifulSoup(yc_web_page, 'html.parser')
-scrap = soup.find_all(name='span', class_='titleline')
+# res = requests.get('https://news.ycombinator.com/news')
+# yc_web_page = res.text
+# soup = BeautifulSoup(yc_web_page, 'html.parser')
 # scrap = soup.find_all(name='span', class_='titleline')
+# # scrap = soup.find_all(name='span', class_='titleline')
 
-data = [[s.text] for s in scrap]
-links = [c.find(name='a').get('href') for c in scrap]
-points = [int(p.text.split()[0]) for p in soup.find_all(name='span', class_='score')]
+    
+
+# data = [[s.text] for s in scrap]
+# links = [c.find(name='a').get('href') for c in scrap]
+# points = [int(p.text.split()[0]) for p in soup.find_all(name='span', class_='score')]
 
 
-for i in range(len(data) - 1):
-    data[i].append(links[i])
-    data[i].append(points[i])
-print(data[len(data) -1])
+# for i in range(len(data) - 1):
+#     data[i].append(links[i])
+#     data[i].append(points[i])
+# print(data)
 
-sorted_data = data.sort(key=lambda data: data[0][2] )
-print(sorted_data)
+# sorted_data = data.sort(key=lambda data: data[2] )
+# print(sorted_data)
+
+
+#TOP MOVIES
+
+res = requests.get('https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2')
+
+soup = BeautifulSoup(res.text, 'html.parser')
+
+
+all_movies = [t.text for t in soup.find_all('h3', class_='title')]
+all_movies = all_movies[::-1]
+with open('movies.txt','w',encoding='UTF8') as file:
+
+
+    for m in all_movies:
+        file.write(m+'\n')
+        print(m)
+
+file.close()
